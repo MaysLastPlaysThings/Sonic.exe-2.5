d9147715342c5b06f71f31e0533d51fc5d268fa4
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-03-18 09:10:46 +0200

    hi

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -60,7 +60,7 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
-		#if mobile
+		#if android
 		FlxG.android.preventDefaultKeys = [BACK];
 		#end
 		// flixel automatically saves your volume!
